2179f965cacb6c8ad88f9d020af6aac8df1d4dcd
WD Zone <117820644+Lo-pro-file@users.noreply.github.com>
2023-12-18 03:00:24 +0530

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -36,18 +36,18 @@ async def get_bot_logs(client: Bot, m: Message):
             await m.reply_document(
                 bot_log_path,
                 quote=True,
-                caption="<b>Ini Logs Bot ini</b>",
+                caption="<b>Here are this Bot's Logs</b>",
             )
         except Exception as e:
             os.remove(bot_log_path)
             LOGGER(__name__).warning(e)
     elif not os.path.exists(bot_log_path):
-        await m.reply_text("❌ <b>Tidak ada log yang ditemukan!</b>")
+        await m.reply_text("✘ <b>No logs found!</b>")
 
 
 @Bot.on_message(filters.command("vars") & filters.user(ADMINS))
 async def varsFunc(client: Bot, message: Message):
-    Man = await message.reply_text("Tunggu Sebentar...")
+    Man = await message.reply_text("Wait a minute...")
     text = f"""<u><b>CONFIG VARS</b></u> @{client.username}
 APP_ID = <code>{APP_ID}</code>
 API_HASH = <code>{API_HASH}</code>
